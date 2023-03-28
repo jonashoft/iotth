c2f9294bcf2317ecec70b653cc61d054ca71aef0
Jonas Höft <jonashoft@live.dk>
2023-03-28 15:50:33 +0200

    added files

---
+++ mqttPublisher.py
@@ -0,0 +1,83 @@
+import paho.mqtt.client as paho, os
+import urllib.parse as urlparse
+import keyboard
+import time
+import random
+from enum import Enum
+
+# Define event callbacks
+def on_connect(mqttc, obj, flags, rc):
+    print("flags, rc: " + str(flags) + " " + str(rc))
+
+def on_message(mqttc, obj, msg):
+    print("Recieved message: " + msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+def on_publish(mqttc, obj, msg_id):
+    print("msg_id: " + str(msg_id))
+
+def on_subscribe(mqttc, obj, msg_id, granted_qos):
+    print("Subscribed: " + str(msg_id) + " " + str(granted_qos))
+
+def on_log(mqttc, obj, level, log_string):
+    print(log_string)
+
+#Create a client instance
+mqttc = paho.Client()
+
+# Assign event callbacks
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_publish = on_publish
+mqttc.on_subscribe = on_subscribe
+
+# Parse CLOUDMQTT_URL (or fallback to localhost)
+url_str = os.environ.get('CLOUDMQTT_URL', 'mqtt://m20.cloudmqtt.com')
+url = urlparse.urlparse(url_str)
+topic = 'jonashoft/'
+tempTopic = topic + 'temperature'
+humTopic = topic + 'humidity'
+presTopic = topic + 'pressure'
+
+mqttc.username_pw_set('wioomzqz', "E7AFAC-lUlUB")
+mqttc.connect('m20.cloudmqtt.com', 10889)
+
+mqttc.subscribe(topic, 0)
+mqttc.publish(topic, 'Getting started ...')
+
+# Continue the loop; exit if an error occurs
+rc = 0
+message = ''
+
+while (rc == 0):   
+    rc = mqttc.loop(.2)
+    time.sleep(0.5)
+
+    dataToSend = random.randint(1,3)
+    dataValue = random.randint(25, 35)
+    match dataToSend:
+        case 1:
+            print("temperature")
+            mqttc.publish(tempTopic, f'temperature {dataValue}')
+        case 2:
+            print("humidity")
+            mqttc.publish(humTopic, f'humidity {dataValue}')
+        case 3:
+            print("pressure")
+            mqttc.publish(presTopic, f'pressure {dataValue}')
+    
+    # keypressed = keyboard.read_key()
+    # if keypressed == 'esc':
+    #     print("fuck frede")
+    #     message = input('message >> ')
+    #     if (message != ''):
+    #         mqttc.publish(topic, message)
+        
+    # if (topic == 'exit') and (message == 'exit'):
+    #     break
+    
+print("rc: " + str(rc))
+
+# Disconnect the broker
+mqttc.disconnect()
+print("Bye!")
+
---
+++ mqttSubscriber_temp.py
@@ -0,0 +1,55 @@
+import paho.mqtt.client as paho, os
+import urllib.parse as urlparse
+import keyboard
+import time
+
+# Define event callbacks
+def on_connect(mqttc, obj, flags, rc):
+    print("flags, rc: " + str(flags) + " " + str(rc))
+
+def on_message(mqttc, obj, msg):
+    print("Recieved message: " + msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+def on_publish(mqttc, obj, msg_id):
+    print("msg_id: " + str(msg_id))
+
+def on_subscribe(mqttc, obj, msg_id, granted_qos):
+    print("Subscribed: " + str(msg_id) + " " + str(granted_qos))
+
+def on_log(mqttc, obj, level, log_string):
+    print(log_string)
+
+#Create a client instance
+mqttc = paho.Client()
+
+# Assign event callbacks
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_publish = on_publish
+mqttc.on_subscribe = on_subscribe
+
+# Parse CLOUDMQTT_URL (or fallback to localhost)
+url_str = os.environ.get('CLOUDMQTT_URL', 'mqtt://m20.cloudmqtt.com')
+url = urlparse.urlparse(url_str)
+topic = 'jonashoft/'
+tempTopic = topic + 'temperature'
+
+mqttc.username_pw_set('wioomzqz', "E7AFAC-lUlUB")
+mqttc.connect('m20.cloudmqtt.com', 10889)
+
+mqttc.subscribe(tempTopic, 0)
+mqttc.publish(tempTopic, 'Temperature published')
+
+# Continue the loop; exit if an error occurs
+rc = 0
+message = ''
+
+while (rc == 0):   
+    rc = mqttc.loop(.2)
+    
+print("rc: " + str(rc))
+
+# Disconnect the broker
+mqttc.disconnect()
+print("Bye!")
+
